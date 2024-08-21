572f956605890763d2318256557f315f7a62ee8f
KBlackwell <43302501+KRBlackwell@users.noreply.github.com>
2024-08-21 09:37:52 -0400

    clean up comments

--- lambda_s3_trigger.py
+++ lambda_s3_trigger.py
@@ -3,6 +3,7 @@
 # lambda triggered by uploading to an s3 bucket: https://www.youtube.com/watch?v=OJrxbr9ebDE
 #     this guy uses the aws console
 #event is where you will pass in important metadata that we need to call back into s3 with
+#event type when setting up lambda will be PUT for aws console and sdk, not POST
 #the event is a json file, we're interested in s3 bucket name (s3,bucket/name) and file name (s3/object/key)
 import json
 import boto3
@@ -15,9 +16,7 @@ def lambda_handler(event, context):
    key = event['Records'][0]['s3']['object']['key']
    print(bucket)
    print(key)
-    return{
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
-#event type will be PUT for aws console and sdk, not POST
+
+
+
     
